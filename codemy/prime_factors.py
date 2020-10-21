6f7656351784ffb77cfc00aaba5622e5e3e9e8f4
olarinde1 <olarindeallitaiwo@gmail.com>
2020-10-21 15:33:21 +0100

    commited changes made to the prime_factors.py file

--- codemy/prime_factors.py
+++ codemy/prime_factors.py
@@ -62,3 +62,98 @@ multiplication_table(5)
 
 multiplication_table(8)	
 # Should print: 8x1=8 8x2=16 8x3=24
+print()
+print("--------------------------------------------This is another code----------------------------------------------------")
+print()
+
+
+def square(n):
+    return n*n
+
+def sum_squares(x):
+    sum = 0
+    for n in range(x):
+        sum += square(n)
+    return sum
+print(sum_squares(10)) # Should be 285
+
+# use for loops when there is a sequence of elements you want to iterate
+# use while loops when you want to repeat an action until a condition changes/a condition is false
+
+
+product = 1
+for n in range(1, 11):
+    product *= n
+
+print(product) 
+
+def to_celcuis(x):
+    return (x - 32) * 5/9
+
+for x in range(0, 101, 10):
+    print(x, to_celcuis(x))
+
+print()
+print("--------------------------------------------This is another code----------------------------------------------------")
+print()
+
+for left in range(7):
+    for right in range(left, 7):
+        print("[" + str(left) + "|" + str(right) + "]", end=" ")
+    print()
+
+teams = ['Dragons', 'Wolves', 'Pandas', 'Unicorns']
+
+for home_teams in teams:
+    for away_teams in teams:
+        if home_teams != away_teams:
+            print(home_teams + ' vs ' + away_teams)
+
+print()
+print("--------------------------------------------This is another code----------------------------------------------------")
+print()
+
+def greet_friends(friends):
+    for friend in friends:
+        print("Hi " + friend)
+
+greet_friends(['Taylor', 'Luisa', 'Jamal', 'Eli'])
+print()
+greet_friends('Alli')
+print()
+greet_friends(['Alli'])
+print()
+
+def factorial(n):
+    if n < 2:
+        return 1
+    return n * factorial(n -1)
+
+print(factorial(9))
+
+def factorial(n):
+    print("Factorial called with ", str(n))
+    if n < 2: 
+        print('Returning 1')
+        return 1
+    result = n * factorial(n - 1)
+    print("Returning " + str(result) + " for factorial of ", str(n))
+    return result
+
+factorial(9)
+
+print()
+print()
+
+def is_power_of(number, base):
+  # Base case: when number is smaller than base.
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+    return 1
+
+  # Recursive case: keep dividing number by base.
+  return is_power_of(number, base)
+
+print(is_power_of(8,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
